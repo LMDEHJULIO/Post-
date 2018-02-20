a14a9a344635caad976701dd1eefe17ec9a58b9a
LMDEHJULIO <juliodevelops@gmail.com>
2018-02-20 17:12:50 -0500

    Edit working

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,6 +2,7 @@ class PinsController < ApplicationController
   before_action :find_pin, only: [:show, :edit, :update, :destroy]
 
   def index
+    @pins = Pin.all.order("created_at DESC")
   end
 
   def show
@@ -22,8 +23,16 @@ class PinsController < ApplicationController
   end
 
   def edit
+
   end
 
+  def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: "Pin was successfully updated"
+    else
+      render "edit"
+    end
+  end
 
 
   def destroy
